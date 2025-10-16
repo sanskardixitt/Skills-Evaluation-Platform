ac26f00ae3b8aceacfd76a4fe3665b064bb33606
sanskardixitt <143865710+sanskardixitt@users.noreply.github.com>
2025-10-16 23:54:09 +0530

    added logout

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -11,5 +11,6 @@ const { authenticate } = require("../middleware/auth");
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", authenticate, getProfile);
+router.get("/logout", logout);
 
 module.exports = router;
